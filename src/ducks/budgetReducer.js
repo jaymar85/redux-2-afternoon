c1e22d410b289e260dc5c85c4d84dfbe0b00c2c5
Jeremy Mark <jeremymark@Christinas-MacBook-Pro.local>
2019-10-10 17:29:13 -0500

    project complete

---
+++ src/ducks/budgetReducer.js
@@ -0,0 +1,79 @@
+import axios from 'axios';
+
+// ACTION TYPE
+const REQUEST_BUDGET_DATA = 'REQUEST_BUDGET_DATA';
+const ADD_PURCHASE = 'ADD_PURCHASE';
+const REMOVE_PURCHASE = 'REMOVE_PURCHASE';
+
+const initialState = {
+    purchases: [],
+    budgetLimit: null,
+    loading: false
+}
+
+export function requestBudgetData() {
+    let data = axios.get('/api/budget-data').then(res => res.data)
+    return {
+        type: REQUEST_BUDGET_DATA,
+        payload: data
+    }
+}
+export function addPurchase(price, description, category) {
+    let data = axios.post('/api/budget-data/purchase', {
+        description,
+        price,
+        category
+    }).then(res => res.data);
+    return {
+        type: ADD_PURCHASE,
+        payload: data
+    }
+}
+export function removePurchase(id) {
+    let data = axios.delete(`/api/budget-data/purchase/${id}`).then(res => res.data);
+    return {
+        type: REMOVE_PURCHASE,
+        payload: data
+    }
+}
+
+// REDUCER
+export default function budgetReducer(state = initialState, action) {
+    switch(action.type) {
+    case REQUEST_BUDGET_DATA + '_PENDING':
+        return {
+            ...state, 
+            loading: true
+        }
+    case REQUEST_BUDGET_DATA + '_FULFILLED':
+        return {
+            ...state,
+            ...action.payload,
+            loading: false
+        }
+    case ADD_PURCHASE + '_PENDING':
+        return {
+            ...state, 
+            loading: true
+        }
+    case ADD_PURCHASE + '_FULFILLED':
+        return {
+            ...state,
+            purchases: action.payload,
+            loading: false
+        }
+    case REMOVE_PURCHASE + '_PENDING':
+        return {
+            ...state, 
+            loading: true
+        }
+    case REMOVE_PURCHASE + '_FULFILLED':
+        return {
+            ...state,
+            purchases: action.payload,
+            loading: false
+        }
+    default:
+        return state;
+    }
+}
